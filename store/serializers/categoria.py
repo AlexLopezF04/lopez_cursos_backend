28f1457c94f9e1bb94b1dd4bbb6c070b5e18c2dc
AlexLopezF04 <j.alexander.lopez.f@gmail.com>
2026-06-07 23:53:04 -0500

    feat: auto-generar slug desde nombre en CategoriaSerializer

--- store/serializers/categoria.py
+++ store/serializers/categoria.py
@@ -1,3 +1,4 @@
+from django.utils.text import slugify
 from rest_framework import serializers
 from store.models import Categoria
 
@@ -5,3 +6,14 @@ class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model  = Categoria
         fields = ['id', 'nombre', 'slug']
+        extra_kwargs = {'slug': {'required': False}}
+
+    def create(self, validated_data):
+        if 'slug' not in validated_data or not validated_data['slug']:
+            validated_data['slug'] = slugify(validated_data['nombre'])
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if 'slug' not in validated_data or not validated_data['slug']:
+            validated_data['slug'] = slugify(validated_data.get('nombre', instance.nombre))
+        return super().update(instance, validated_data)
